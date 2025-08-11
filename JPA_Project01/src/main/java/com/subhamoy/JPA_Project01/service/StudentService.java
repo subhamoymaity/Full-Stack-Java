2c867863287175e6bcb584aae6175e273c0f8be9
subhamoymaity <cse22029@cemk.ac.in>
2025-08-12 00:17:01 +0530

    comment

--- JPA_Project01/src/main/java/com/subhamoy/JPA_Project01/service/StudentService.java
+++ JPA_Project01/src/main/java/com/subhamoy/JPA_Project01/service/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -16,8 +17,33 @@ public class StudentService {
    public Student saveStudent(Student student) {
       return studentRepo.save(student);
    }
-   public List<Student> getAllStudentData(){
+
+   public List<Student> getAllStudentData() {
       List<Student> list = studentRepo.findAll();
-      return  list;
+      return list;
+   }
+
+   public Student getStudentById(Integer id)
+   {
+      Optional<Student> byId = studentRepo.findById(id);
+      Student student = byId.get();
+      return student;
+
+   }
+   public Student updateStudent(Integer id,Student student) {
+      Optional<Student> studentById = studentRepo.findById(id);
+      Student stud = null;
+      if(studentById.isPresent())
+      {
+         Student dbStudentObject = studentById.get();
+         dbStudentObject.setAge(student.getAge());
+         dbStudentObject.setEmail(student.getEmail());
+         dbStudentObject.setName(student.getName());
+         stud= studentRepo.save(dbStudentObject);
+      }
+      else {
+         System.out.println("Data not present in db....");
+      }
+      return stud;
    }
 }
