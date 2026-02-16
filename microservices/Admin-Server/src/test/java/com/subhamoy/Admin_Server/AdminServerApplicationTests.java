a9cd5b34dd0a2adbac9dce0c5dd5f5b1fa544fbf
subhamoymaity <cse22029@cemk.ac.in>
2026-02-16 11:56:32 +0530

    microservice admin service

---
+++ microservices/Admin-Server/src/test/java/com/subhamoy/Admin_Server/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhamoy.Admin_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- microservices/Service-Registry/src/main/java/com/subhamoy/Service_Registry/ServiceRegistryApplication.java
+++ microservices/Service-Registry/src/main/java/com/subhamoy/Service_Registry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.subhamoy.Service_Registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
